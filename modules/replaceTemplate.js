6c3b46f3b0d7c025cf1b291e2ec2ca268ffed769
fabiosvd <fabiosalvador85@yahoo.com.br>
2021-08-28 16:58:46 -0300

    created replaceTemplate module

--- modules/replaceTemplate.js
+++ modules/replaceTemplate.js
@@ -1,4 +1,4 @@
-module.exports  = (temp, product) => {
+module.exports = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%PRICE%}/g, product.price);
@@ -8,7 +8,7 @@ module.exports  = (temp, product) => {
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
 
-  if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
   return output;
-  
+
 }
